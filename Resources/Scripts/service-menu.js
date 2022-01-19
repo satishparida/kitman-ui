b1cd249060e58f52b160f2696835b7a3a67936e6
Satish <satish.2441@gmail.com>
2022-01-19 20:59:32 +0530

    cart checkout

--- Resources/Scripts/service-menu.js
+++ Resources/Scripts/service-menu.js
@@ -7,6 +7,7 @@ function addItem(itemId) {
     addButton.classList.add("hide");
     mixedButton.classList.remove("hide");
     controlHeaderCartCount('add');
+    centralCartController('add', itemId)
 }
 
 function addItemPlus(itemId) {
@@ -15,6 +16,7 @@ function addItemPlus(itemId) {
     itemCount = itemCount + 1;
     mixedButtonItemCount.innerHTML = itemCount;
     controlHeaderCartCount('add');
+    centralCartController('add', itemId)
 }
 
 function removeItemMinus(itemId) {
@@ -33,6 +35,7 @@ function removeItemMinus(itemId) {
     }
     
     controlHeaderCartCount('remove');
+    centralCartController('remove', itemId)
 }
 
 function controlHeaderCartCount(methode){
@@ -74,34 +77,98 @@ function controlHeaderCartCount(methode){
 }
 
 function centralCartController(methode, itemId){
-    
+
     if (methode == 'add') {
-        if (headerCartCount == 0){
-            emptyCart.classList.add("hide");
-            centralCartCountSpan.innerHTML = headerCartCountSpan.innerHTML;
+        if (document.getElementById("cartItem_"+itemId) == null) {
+
             if (document.getElementById('item_cat_'+itemId).innerHTML == 'Veg' ) {
                 div_cat =  '<div class="cart-item-isveg"></div>';
             }
             else {
                 div_cat =  '<div class="cart-item-isnonveg"></div>';
             }
-            cartItems.innerHTML =+ '<div class="cart-item" id="cartItem_' + itemId + '">' +
-                                        '<div class="cart-item-detail" id="cartItemDetail_' + itemId + '">' +
-                                        div_cat +
-                                            '<div class="cart-item-name">French Fries</div>' +
-                                        '</div>' +
-                                        '<div class="cart-item-quantity">'+
-                                            '<i class="fa fa-times"></i>' +
-                                            '<span>1</span>' +
-                                        '</div>' +
-                                        '<div class="cart-item-price">' +
-                                            '<span>₹</span>' +
-                                            '<span>100</span>' +
-                                        '</div>'+
-                                    '</div>';
 
+            var div_name = document.getElementById('item_name_'+itemId).innerHTML;
+            var div_cost = document.getElementById('item_cost_'+itemId).innerHTML;
 
-            nonEmptyCart.classList.remove("hide");
+            var cartItems = document.getElementById("cart-items")
+
+            item_div =  '<div class="cart-item" id="cartItem_' + itemId + '">' +
+                            '<div class="cart-item-detail" id="cartItemDetail_' + itemId + '">' +
+                            div_cat +
+                                '<div class="cart-item-name">'+div_name+'</div>' +
+                            '</div>' +
+                            '<div class="cart-item-quantity">'+
+                                '<i class="fa fa-times"></i>' +
+                                '<span id="cartItemQuantity_' + itemId + '">1</span>' +
+                            '</div>' +
+                            '<div class="cart-item-price">' +
+                                '<span>₹</span>' +
+                                '<span id="cartItemPrice_' + itemId + '">'+div_cost+'</span>' +
+                            '</div>'+
+                        '</div>';
+            cartItems.insertAdjacentHTML('beforeend', item_div);
         }
+        else {
+            var div_quantity = parseInt(document.getElementById('cartItemQuantity_'+itemId).innerHTML);
+            div_quantity = div_quantity +1;
+            document.getElementById('cartItemQuantity_'+itemId).innerHTML = div_quantity;
+            var price = parseFloat(document.getElementById('item_cost_'+itemId).innerHTML);
+            var div_cost = div_quantity * price;
+            document.getElementById('cartItemPrice_'+itemId).innerHTML = div_cost;
+        }
+
     }
+
+    else if (methode == 'remove') {
+        var div_quantity = parseInt(document.getElementById('cartItemQuantity_'+itemId).innerHTML);
+        if (div_quantity > 1) {
+            div_quantity = div_quantity -1;
+            document.getElementById('cartItemQuantity_'+itemId).innerHTML = div_quantity;
+            var price = parseFloat(document.getElementById('item_cost_'+itemId).innerHTML);
+            var div_cost = div_quantity * price;
+            document.getElementById('cartItemPrice_'+itemId).innerHTML = div_cost;
+        }
+        else {
+            document.getElementById('cartItem_'+itemId).remove();
+        }
+        
+    }
+
+    var allCartItems = document.getElementsByClassName('cart-item');
+    var subtotal =0;
+    for (i=0; i<allCartItems.length; i++) {
+        var item = parseFloat(allCartItems[i].getElementsByClassName('cart-item-price')[0].getElementsByTagName('span')[1].innerText);
+        subtotal = subtotal + item;
+    }
+    document.getElementById('cartSubtotal').innerHTML = "₹ "+subtotal;
+}
+
+function cartCheckout() {
+    var allCartItems = document.getElementsByClassName('cart-item');
+    payload =[]
+    var totalQuantity = 0;
+
+    payload.push( {"noofrows": allCartItems.length});
+    
+    for (i=0; i<allCartItems.length; i++) {
+        var item = allCartItems[i];
+        var itemId = item.id.split('_')[1].trim();
+        var itemName = item.getElementsByClassName('cart-item-detail')[0].getElementsByTagName('div')[1].innerText;
+        var vegiterian = item.getElementsByClassName('cart-item-detail')[0].getElementsByTagName('div')[0].className.split('-')[2] == 'isveg';
+        var quantity = parseInt(item.getElementsByClassName('cart-item-quantity')[0].getElementsByTagName('span')[0].innerText);
+        totalQuantity = totalQuantity + quantity;
+
+        item = {"itemId": itemId, 
+                "itemName": itemName, 
+                "vegiterian": vegiterian, 
+                "quantity": quantity}
+
+        payload.push(item);
+    }
+    payload.push( {"totalQuantity": totalQuantity});
+    var totalCost = parseFloat(document.getElementById('cartSubtotal').innerText.split(' ')[1]);
+    payload.push( {"totalCost": totalCost});
+    payloadJSON = JSON.stringify(payload)
+    console.log(payloadJSON);
 }
